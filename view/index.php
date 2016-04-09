63ea1cff7438b22db80cd28a679a87d822f6907c
Cássio Santos <sss.cassio@gmail.com>
2016-04-09 17:30:39 -0300

    Modificacoes

--- view/index.php
+++ view/index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD:view/index.php
 
-=======
->>>>>>> e1f2d59a3ee9977bc0ed02971704eeb7f66132db:view/views/index.php
 <head>
 
     <meta charset="utf-8">
@@ -21,21 +18,12 @@
     <!-- Custom CSS -->
     <link href="css/modern-business.css" rel="stylesheet">
     <link href="css/bootstrap-lavish.css" rel="stylesheet">
-<<<<<<< HEAD:view/index.php
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-=======
-    <link rel="stylesheet" href="assets/owlcarousel/owl.carousel.css"> <!-- arrumacao e acho que animacao do slide -->
->>>>>>> e1f2d59a3ee9977bc0ed02971704eeb7f66132db:view/views/index.php
 
     <script src="js/modernizr.js"></script> <!-- Modernizr -->
 
     <!-- Custom Fonts -->
-<<<<<<< HEAD:view/index.php
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-=======
-    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="css/style.css" rel="stylesheet"> <!-- IMPORTANTE ! SE TIRAR ISSO NAO FICA COMO BARRA LATERAL! -->
->>>>>>> e1f2d59a3ee9977bc0ed02971704eeb7f66132db:view/views/index.php
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -48,21 +36,20 @@
 
 <body>
 
-<<<<<<< HEAD:view/index.php
 <?php
     include("cabecalhoDeslogado.php");
-?>
-=======
-    <?php
-      include("cabecalho.php");
-     ?>
->>>>>>> e1f2d59a3ee9977bc0ed02971704eeb7f66132db:view/views/index.php
+  ?>
 
 
-<br><br>
 
     <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner">
@@ -75,7 +62,7 @@
             <div class="item">
                 <div class="fill" style="background-image:url('http://lorempixel.com/1900/1080/people/2');"></div>
                 <div class="carousel-caption">
-                  <a href="filtro.php" class="btn btn-lg btn-primary">Ver Campanhas</a>
+                  <button type="button" class="btn btn-primary btn-lg"name="button">Ver Campanhas</button>
                 </div>
             </div>
             <div class="item">
@@ -101,7 +88,7 @@
       <!-- Features Section -->
       <div class="row">
           <div class="col-lg-12">
-              <h2 class="page-header">Sobre o Boa Iniciativa</h2>
+              <h1 class="page-header">Sobre o Boa Iniciativa</h2>
           </div>
           <div class="col-md-6">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
@@ -113,49 +100,47 @@
       <!-- /.row -->
         <!-- Marketing Icons Section -->
         <div class="row">
-            <form method="get" action="visualizarCampanha.php">
-                <div class="col-lg-12">
-                    <h2 class="page-header">
-                      Campanhas em Destaque
-                    </h2>
-                </div>
-                <div class="col-md-4">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4>Boa Iniciativa</h4>
-                        </div>
-                        <div class="panel-body">
-                            <img class="img-responsive img-portfolio img-hover" src="http://lorempixel.com/700/450/people/" alt="">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                            <a href="visualizarCampanha.php?campanha=Boa Iniciativa " class="btn btn-primary">Ver Campanha</a>
-                        </div>
+            <div class="col-lg-12">
+                <h2 class="page-header">
+                  Campanhas em Destaque
+                </h2>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Boa Iniciativa</h4>
+                    </div>
+                    <div class="panel-body">
+                        <img class="img-responsive img-portfolio img-hover" src="http://lorempixel.com/700/450/people/" alt="">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-primary">Ver Campanha</a>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                          <h4>Campanha para Mariana</h4>
-                        </div>
-                        <div class="panel-body">
-                            <img class="img-responsive img-portfolio img-hover" src="http://lorempixel.com/700/450/people/5" alt="">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                            <a href="visualizarCampanha.php?campanha=Campanha para Mariana" class="btn btn-primary">Ver Campanha</a>
-                        </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                      <h4>Campanha para Mariana</h4>
+                    </div>
+                    <div class="panel-body">
+                        <img class="img-responsive img-portfolio img-hover" src="http://lorempixel.com/700/450/people/5" alt="">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-primary">Ver Campanha</a>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                          <h4>Campanha contra Corrupção</h4>
-                        </div>
-                        <div class="panel-body">
-                            <img class="img-responsive img-portfolio img-hover" src="http://lorempixel.com/700/450/people/6" alt="">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                            <a href="visualizarCampanha.php?campanha=Campanha contra Corrupção" class="btn btn-primary">Ver Campanha</a>
-                        </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                      <h4>Campanha contra Corrupção</h4>
+                    </div>
+                    <div class="panel-body">
+                        <img class="img-responsive img-portfolio img-hover" src="http://lorempixel.com/700/450/people/6" alt="">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-primary">Ver Campanha</a>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
         <!-- /.row -->
 
@@ -199,7 +184,6 @@
 
 
 
-<<<<<<< HEAD:view/index.php
         <hr>
 
         <!-- Call to Action Section -->
@@ -208,16 +192,13 @@
 
         <hr>
 
+        <?php
+          include('footer.php');
 
-
-
-
-
+         ?>
     </div>
     <!-- /.container -->
 
-    <!-- Footer -->
-    <?php include("footer.php"); ?>
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
@@ -231,7 +212,6 @@
     })
     </script>
 
-<<<<<<< HEAD:view/index.php
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
@@ -248,9 +228,36 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="js/main.js"></script> <!-- Gem jQuery -->
 
+        <script type="text/javascript">
+          $(document).ready(function () {
+            var trigger = $('.hamburger'),
+                overlay = $('.overlay'),
+               isClosed = false;
 
-=======
->>>>>>> e1f2d59a3ee9977bc0ed02971704eeb7f66132db:view/views/index.php
+              trigger.click(function () {
+                hamburger_cross();
+              });
+
+              function hamburger_cross() {
+
+                if (isClosed == true) {
+                  overlay.hide();
+                  trigger.removeClass('is-open');
+                  trigger.addClass('is-closed');
+                  isClosed = false;
+                } else {
+                  overlay.show();
+                  trigger.removeClass('is-closed');
+                  trigger.addClass('is-open');
+                  isClosed = true;
+                }
+            }
+
+            $('[data-toggle="offcanvas"]').click(function () {
+                  $('#wrapper').toggleClass('toggled');
+            });
+          });
+        </script>
 </body>
 
 </html>
