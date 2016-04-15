7f005b0da3d14cf2aa6463ba7e5c71bb559d6bfb
Cássio Santos <sss.cassio@gmail.com>
2016-04-15 09:12:27 -0300

    Tentando corrigir problema do listar materiais, e adicionar mascara no CPF

--- view/views/atender.php
+++ view/views/atender.php
@@ -25,11 +25,11 @@ if(isset($_GET['campanha'])){
           <div class="col-md-6 panel panel-success">
             <h3 class="page-header"> Digite o CPF do usuário para confirmar a doação</h3>
             <script type="text/javascript">
-              $(document).ready(function (){
-                $('#botaoVerDoacoes').click(function(){
-                  document.formVerDoacoes.submit();
-                });
+            $(document).ready(function (){
+              $('#botaoVerDoacoes').click(function(){
+                document.formVerDoacoes.submit();
               });
+            });
             </script>
             <form  name="formVerDoacoes" id="formVerDoacoesCPF" action="atender.php">
               <input type="hidden" id="idCampanha" name="campanha" value="<?php echo $idCampanha; ?>">
@@ -47,10 +47,10 @@ if(isset($_GET['campanha'])){
             </form>
 
           </div>
-            <?php if(isset($_GET['cpf'])){//Se o cpf do usuário for setado
-              echo '<div class="col-md-6 panel panel-success">';
-              $usuario = UsuarioController::getInstance()->buscarUsuario($_GET['cpf']);
-              if($usuario->getCpf() != null){
+          <?php if(isset($_GET['cpf'])){//Se o cpf do usuário for setado
+            echo '<div class="col-md-6 panel panel-success">';
+            $usuario = UsuarioController::getInstance()->buscarUsuario($_GET['cpf']);
+            if($usuario->getCpf() != null){
               echo '<h2 class="page-header">Doações de ';
               if($usuario->getNome() == ""){
                 echo $usuario->getCpf();
@@ -61,34 +61,34 @@ if(isset($_GET['campanha'])){
               $doacoes = AtendenteController::getInstance()->buscarDoacoesPendentesNaCampanha($_GET['cpf'],$_GET['campanha']);
               if(sizeof($doacoes) == 0){
                 echo '<div class="alert alert-warning row alert-dismissible" role="alert" style="margin:10px 0px 10px 0px;">
-                   Usuario não tem processo de doação iniciado nessa campanha, iniciar um agora clicando no botão abaixo.
+                Usuario não tem processo de doação iniciado nessa campanha, iniciar um agora clicando no botão abaixo.
                 </div>';
-                 //Parte de criar nova doação aqui em baixo?>
+                //Parte de criar nova doação aqui em baixo?>
 
 
-                 <?php
+                <?php
               }else{//Listar as doacoes não confirmadas do usuario
                 echo '<div class="table-responsive">';
                 echo '<table class="table table-striped">';
                 echo "<tr>
-                        <th>Data</th>
-                        <th>Confirmar Doação</th>
-                      </tr>";
+                <th>Data</th>
+                <th>Confirmar Doação</th>
+                </tr>";
                 for ($i=0; $i < sizeof($doacoes); $i++) {
 
 
-              ?>
-                <td> <?php echo $doacoes[$i]->getData(); ?></td>
-                <td>
-                  <input type="radio" name="doacao" value="<?php echo $doacoes[$i]->getIdDoacao(); ?>"> Selecionar<br>
-                </td>
+                  ?>
+                  <td> <?php echo $doacoes[$i]->getData(); ?></td>
+                  <td>
+                    <input type="radio" name="doacao" value="<?php echo $doacoes[$i]->getIdDoacao(); ?>"> Selecionar<br>
+                  </td>
 
-              <?php
-              }
-              echo '</div>';
-              echo'</table>';
-              ?>
-              <script type="text/javascript">
+                  <?php
+                }
+                echo '</div>';
+                echo'</table>';
+                ?>
+                <script type="text/javascript">
                 $(document).ready(function(){
                   $('#SelecaoMateriais').hide();
 
@@ -96,15 +96,22 @@ if(isset($_GET['campanha'])){
                     $('#SelecaoMateriais').show();
                   });
 
-
+                  $('#cpfUsuario').inputmask('999.999.999-99', {
+                    "onincomplete": function () {
+                      console.log("incompleto");
+                    },
+                    "oncomplete": function () {
+                      console.log("completo");
+                    }
+                  });
 
 
 
                 });
 
-              </script>
-              <div id="SelecaoMateriais">
-                <?php
+                </script>
+                <div id="SelecaoMateriais">
+                  <?php
                   $materiaisCampanha = AtendenteController::getInstance()->listarMateriaisCampanha($idCampanha);
                   $materiais = AtendenteController::getInstance()->listarMateriais();
 
@@ -114,98 +121,99 @@ if(isset($_GET['campanha'])){
                     $opcoes .= $materiaisCampanha[$i]->getNome();
                     $opcoes .= '</option>';
                   }
-                 ?>
-                 <form id="confirmarDoacao" method="post" action="rotas.php">
-                  <table id="lista" class="table table-striped">
-                    <thead>
-                          <tr>
-                              <th>Item</th>
-                              <th>Quantidade</th>
-                          </tr>
-                    </thead>
-                    <tbody>
-                          <tr>
-                              <td><?php
-                              echo '<select name="material1">';
-                              echo $opcoes;
-                              echo "</select>";
-                               ?>
-                             </td>
-                              <td><input type="number" required name="quantidade1" size="10" /></td>
-                          </tr>
+                  ?>
+                  <form id="confirmarDoacao" method="post" action="rotas.php">
+                    <table id="lista" class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th>Item</th>
+                          <th>Quantidade</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td><?php
+                          echo '<select name="material1">';
+                          echo $opcoes;
+                          echo "</select>";
+                          ?>
+                        </td>
+                        <td><input type="number" required name="quantidade1" size="10" /></td>
+                      </tr>
                     </tbody>
                   </table>
                   <!--Irá armazenar a quantidade de linhas-->
                   <input type="hidden" value="1" name="quantidade_itens" />
                   <input type="hidden" name="botaoConfirmarDoacao"/>
-                  </form>
+                </form>
 
-                  <script type="text/javascript">
-                    var opcoes = '<?php print $opcoes; ?>';
+                <script type="text/javascript">
 
-                    $(document).ready(function (){
-                      $('#mais').click(function(){
+                $(document).ready(function (){
+                  const opcoes = '<?php echo $opcoes; ?>';
 
-                          //recuperando o próximo numero da linha
-                          var next = $('#lista tbody').children('tr').length + 1;
+                  $('#mais').click(function(){
+                    console.log(opcoes);
+                    //recuperando o próximo numero da linha
+                    var next = $('#lista tbody').children('tr').length + 1;
 
-                          //inserindo formulário
-                          $('#lista tbody').append('<tr>' +
-                              '<td><select name="material'+ next +'">'+opcoes+'</select></td>' +
-                              '<td><input type="number" required name="quantidade' + next + '" /></td>' +
-                          '</tr>');
+                    //inserindo formulário
+                    $('#lista tbody').append('<tr>' +
+                    '<td><select name="material'+ next +'">'+opcoes+'</select></td>' +
+                    '<td><input type="number" required name="quantidade' + next + '" /></td>' +
+                    '</tr>');
 
-                          //armazenando a quantidade de linhas ou registros no elemento hidden
-                          $(':hidden').val(next);
+                    //armazenando a quantidade de linhas ou registros no elemento hidden
+                    $(':hidden').val(next);
 
-                          return false;
-                      });
+                    return false;
+                  });
 
-                      $('#enviar').click(function(){
-                          $('#confirmarDoacao').submit();
-                      });
-                    });
+                  $('#enviar').click(function(){
+                    $('#confirmarDoacao').submit();
+                  });
+                });
 
-                  </script>
+                </script>
 
-                  <div class="col-md-6">
-                    <a  class="btn btn-primary btn-block" id="mais">Adicionar Item</a>
-                  </div>
-                  <div class="col-md-6">
-                    <a class="btn btn-primary btn-block" id="enviar">Confirmar Doação</a>
-                  </div>
+                <div class="col-md-6">
+                  <a  class="btn btn-primary btn-block" id="mais">Adicionar Item</a>
+                </div>
+                <div class="col-md-6">
+                  <a class="btn btn-primary btn-block" id="enviar">Confirmar Doação</a>
+                </div>
 
               </div>
               <?php
             }
-            }else{
-                echo '<div class="alert alert-warning row alert-dismissible" role="alert" style="margin:10px 0px 10px 0px;">
-                  <strong>Desculpe!</strong> Usuario não encontrado. Tente efetuar um cadastro rápido no painel ao lado.
-                </div>';
-              }
-              echo '</div>';
-            } ?>
+          }else{
+            echo '<div class="alert alert-warning row alert-dismissible" role="alert" style="margin:10px 0px 10px 0px;">
+            <strong>Desculpe!</strong> Usuario não encontrado. Tente efetuar um cadastro rápido no painel ao lado.
+            </div>';
+          }
+          echo '</div>';
+        } ?>
 
-        </div>
+      </div>
 
-        <?php
-      }else{//Usuario Não atende na campanha
-        ?>
-        <div class="alert alert-warning row alert-dismissible" role="alert" style="margin:10px 0px 10px 0px;">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <strong>Desculpe!</strong> Você não tem permissão de atender nesta campanha.
-        </div>
+      <?php
+    }else{//Usuario Não atende na campanha
+      ?>
+      <div class="alert alert-warning row alert-dismissible" role="alert" style="margin:10px 0px 10px 0px;">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <strong>Desculpe!</strong> Você não tem permissão de atender nesta campanha.
+      </div>
 
-        <?php
+      <?php
 
-      }
-
-    }else{
-      header('location:atendente.php');
     }
 
-    ?>
-  </div>
+  }else{
+    header('location:atendente.php');
+  }
+
+  ?>
+</div>
 </div>
 </div>
 
