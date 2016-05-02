23301af26215fb68c20f4b631dacd3748fd7a680
Renato Santos <natofsa@yahoo.com.br>
2016-05-02 01:11:46 -0300

    Validalção de cpf no recuperar Senha

--- view/views/recuperarSenha.php
+++ view/views/recuperarSenha.php
@@ -4,6 +4,8 @@ if(isset($_POST['cpf']) && isset($_POST['senha'])){
 	header('location:home.php');
 }
 ?>
+<script type="text/javascript" src="../js/login.js"></script>
+<script src="../js/jquery.maskedinput.js" type="text/javascript"></script>
 <br><br>
   <div class="container">
     <div class="row">
@@ -20,9 +22,12 @@ if(isset($_POST['cpf']) && isset($_POST['senha'])){
           <form method="POST" action="rotas.php">
           <div class="form-group">
             <label >Seu CPF<font color="FF0000">*</font></label>
-            <input type="text" class="form-control" name="cpf" required data-validation-required-message="Por favor, digite seu CPF." placeholder="Ex.: 00000000000" required>
+            <input id="cpflogin" type="text" class="form-control" name="cpf" required data-validation-required-message="Por favor, digite seu CPF." placeholder="Ex.: 00000000000" required>
           </div>
+          <div class="col-md-6" id="cpfloginerror" ></div>
+          <div class="row">
           <button type="submit" name="botaoRecuperarSenha" class="btn btn-primary">Recuperar</button>
+          </div>
           <br><br>
         </form>
         </div>
