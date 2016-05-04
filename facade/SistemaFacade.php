a021b67b98ce0ee9316cec691dc032e34f1077cd
Cássio Santos <sss.cassio@gmail.com>
2016-05-04 00:37:32 -0300

    Listagem de metas parcialmente funcionando, falta finalizar a campanha quando atingir todas as metas materiais

--- facade/SistemaFacade.php
+++ facade/SistemaFacade.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."controller/SistemaController.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."database/DoacaoDAO.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."database/MetaDAO.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."database/MaterialDAO.php");
+
 
   class  SistemaFacade{
 
@@ -19,14 +23,57 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."controller/SistemaCo
     public function cadastrarNovoUsuario($nome,$cpf,$email,$senha,$nascimento,$cep,$estado,$bairro,$cidade,$longradouro,$numero,$complemento,$sexo){
       return  SistemaController::getInstance()->cadastrarUsuario($nome,$cpf,$email,$senha,$nascimento,$cep,$estado,$bairro,$cidade,$longradouro,$numero,$complemento,$sexo);
     }
- 
-  public function editarPerfil($nome, $email, $senha, $foto, $sexo, $nacimento, $classificacao, $cep, $estado, 
-  $bairro, $cidade, $logadouro, $numero, $complemento, $cpf, $bloqueado, $dataBloqueio, $latitude, $longitude){ 
-  
-	return SistemaController::getInstance()->editarPerfil($nome, $email, $senha, $foto, $sexo, $nacimento, $classificacao,   
+
+  public function editarPerfil($nome, $email, $senha, $foto, $sexo, $nacimento, $classificacao, $cep, $estado,
+  $bairro, $cidade, $logadouro, $numero, $complemento, $cpf, $bloqueado, $dataBloqueio, $latitude, $longitude){
+
+	return SistemaController::getInstance()->editarPerfil($nome, $email, $senha, $foto, $sexo, $nacimento, $classificacao,
     $cep, $estado, $bairro, $cidade, $logadouro, $numero, $complemento, $cpf, $bloqueado, $dataBloqueio, $latitude, $longitude);
   }
-  
+
+  public function printProgressMeta($idCampanha){
+    $doacoesNaCampanha = DoacaoDAO::getInstance()->buscarDoacoesDaCampanha($idCampanha);
+    $metas = MetaDAO::getInstance()->buscarMetasCampanha($idCampanha);
+
+    $materiaisDoados = array();
+    for ($i=0; $i < sizeof($doacoesNaCampanha); $i++) {
+      if ($doacoesNaCampanha[$i]->getConfirmado()) {
+        $materiaisNaDoacao = DoacaoMaterialDAO::getInstance()->listarMateriaisDaDoacao($doacoesNaCampanha[$i]->getIdDoacao());
+        for ($j=0; $j < sizeof($materiaisNaDoacao); $j++) {
+          $materiaisDoados[] =$materiaisNaDoacao[$j];
+        }
+      }
+    }
+
+    $materiaisTotais = array();
+
+    for ($i=0; $i < sizeof($materiaisDoados); $i++) {
+      if (array_key_exists($materiaisDoados[$i]->getNomeMaterial(),$materiaisTotais)) {
+        $materiaisTotais[$materiaisDoados[$i]->getNomeMaterial()] += $materiaisDoados[$i]->getQuantidade();
+      }else{
+        $materiaisTotais[$materiaisDoados[$i]->getNomeMaterial()] = $materiaisDoados[$i]->getQuantidade();
+      }
+    }
+
+    for ($i=0; $i < sizeof($metas) ; $i++) {
+      $material = MaterialDAO::getInstance()->buscarMaterial($metas[$i]->getCodMaterial());
+      if (array_key_exists($material->getNome(),$materiaisTotais)){
+        $valor = $materiaisTotais[$material->getNome()];
+      }else{
+        $valor = 0;
+      }
+
+      $max = $metas[$i]->getQuantidade();
+      $tamanho = ($valor/$max)*100;
+      echo '<h4>'.$material->getNome().'</h3>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="'.$tamanho.'" aria-valuemin="0" aria-valuemax="100" style="width:'.$tamanho.'%"> '.$valor.'/'.$max.'</div>
+      </div>';
+    }
+
+  }
+
+
   public function excluirPerfil($cpf){
 	return SistemaController::getInstance()->excluirPerfil($cpf);
   }
--- view/views/campanha.php
+++ view/views/campanha.php
@@ -2,10 +2,10 @@
 <?php
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."controller/CriadorController.php");
-if(isset($_SESSION['cpf']) && isset($_SESSION['senha'])){
+session_start();
+if( isset($_SESSION['cpf']) && isset ($_SESSION['senha']) ){//Verifica se já está logado
   include("cabecalhologado.php");
-}
-else{
+}else {
   include("cabecalho.php");
 }
 
@@ -25,6 +25,7 @@ else{
     <?php
     require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."facade/UsuarioFacade.php");
     require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."controller/SistemaController.php");
+    require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."facade/SistemaFacade.php");
 
     $id = $_GET['campanha'];
     $campanha = UsuarioFacade::getInstance()->verCampanha($id);
@@ -43,134 +44,121 @@ else{
         echo '<span class="label label-danger">Finalizada</span>';
       }?>
     </span> </h2>
+    <div class="row">
 
-    <div class="col-xs-6">
-      <?php if($campanha->getImagem() == "" || $campanha == "default.jpg"){
-        echo '<img src="../img/campanha.png" class="img-responsive img-rounded" alt="" />';
-      }else{
-        echo '<img src="'.$campanha->getImagem().'" class="img-responsive img-rounded" alt="" />';
-      } ?>
+      <!--Imagem-->
+      <div class="col-xs-6">
+        <?php if($campanha->getImagem() == "" || $campanha == "default.jpg"){
+          echo '<img src="../img/campanha.png" class="img-responsive img-rounded" alt="" />';
+        }else{
+          echo '<img src="'.$campanha->getImagem().'" class="img-responsive img-rounded" alt="" />';
+        } ?>
+      </div>
+
+      <div class="col-12-6 col-md-6">
+        <div class="col-xs-12">
+          <h3>Data de inicio: <?php echo date("d/m/Y", strtotime($campanha->getDataInicio()));  ?></h3>
+        </div>
+        <div class="col-xs-12">
+          <h3>Data de Fim: <?php echo date("d/m/Y", strtotime($campanha->getDataFim()));?></h3>
+        </div>
+        <?php if(!$campanha->getFinalizaPorData()){ ?>
+        <div class="col-xs-12">
+          <h3>Meta:</h3>
+          <?php SistemaFacade::getInstance()->printProgressMeta($campanha->getIdCampanha()); ?>
+        </div>
+        <?php } ?>
+      </div>
     </div>
 
 
     <div class="row">
-      <div class="col-xs-6">
-        <h3>Data de inicio: <?php echo date("d/m/Y", strtotime($campanha->getDataInicio()));  ?></h3>
-      </div>
-      <div class="col-xs-6">
-        <h3>Data de Fim: <?php echo date("d/m/Y", strtotime($campanha->getDataFim()));?></h3>
-      </div>
-      <?php  if(!($campanha->getFinalizaPorData())){
-        require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."controller/DoadorController.php");
-        require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."database/MaterialDAO.php");
-        //verifica as metas de uma campanha
-        $todasmetas = DoadorController::getInstance()->verificarMeta($campanha->getIdCampanha());
+      <div class=" panel panel-success col-md-6" style=" padding:20px;">
+        <h2 class="page-header">Descrição</h2>
+        <?php echo $campanha->getDescricao(); ?>
 
-        for ($i = 0; $i < count($todasmetas); $i++) {
-          //pega uma das metas
-          $meta = $todasmetas[$i];
-          //ver qual o material deseja ser arrecadado
-          $material = MaterialDAO::getInstance()->buscarMaterial($meta->getCodMaterial());
-
-
-          ?>
-          <div class="col-xs-6">
-            <h3>Meta <?php $material->getNome(); ?>:</h3> <?php echo $meta->getQuantidade();?>
-          </div>
-          <?php }//fecha for
-        }//fecha if ?>
-      </div><div class="row">
-        <img src="../img/logobi.png" class="img-rounded img-responsive center-block" alt="" width="100px" height="100px" />
       </div>
-      <div class="row">
-        <div class="container-fluid panel panel-success col-md-6" style=" padding:20px;">
-          <h2 class="page-header">Descrição</h2>
-          <?php echo $campanha->getDescricao(); ?>
-          <br/>
-          <br/>
-          <br/>
+
+      <script type="text/javascript" src="../js/campanha.js"></script>
+
+      <div class=" panel panel-success col-md-6" style="padding:20px;">
+        <h2 class="page-header">Ações</h2>
+        <a href="usuario.php?cpf=<?php echo $campanha->getCriadorCpf();?>">Ver criador</a><br/>
+        <a href="">Convidar Amigo</a><br/>
+        <?php if(isset($_SESSION['cpf']) && isset($_SESSION['senha'])){
+          echo '<a id="botaoDenunciar">Denunciar</a>';
+        } ?>
+        <div class="panel panel-primary" id="formDenuncia" >
+          <form action="rotas.php" method="post">
+            <input type="hidden" name="idCampanha" value="<?php echo $campanha->getIdCampanha(); ?>">
+            <input type="hidden" name="cpf" value="<?php echo $_SESSION['cpf']; ?>">
+            <div class="form-group col-md-12">
+              <label>Motivo</label><br>
+              <input type="radio" name="motivo" value="Campanha Duvidosa" checked> Campanha Duvidosa <br>
+              <input type="radio" name="motivo" value="Usuário Falso	"> Usuário Falso <br>
+              <input type="radio" name="motivo" value="Informações Incompletas"> Informações Incompletas	<br>
+
+            </div>
+            <div class="form-group col-md-12">
+              <label>Descrição</label>
+              <input type="text-area" class="form-control" name="descricao" required placeholder="Descricao">
+            </div>
+            <div class="row text-center">
+              <div class="col-md-6">
+                <input type="submit" class="btn btn-primary btn-block" style="margin:5px 0px 5px 0px;" name="botaoEnviarDenuncia" value="Denunciar">
+              </div>
+            </div>
+          </form>
+
         </div>
 
-        <script type="text/javascript" src="../js/campanha.js"></script>
-
-        <div class="container-fluid panel panel-success col-md-6" style="padding:20px;">
-          <h2 class="page-header">Ações</h2>
-          <a href="usuario.php?cpf=<?php echo $campanha->getCriadorCpf();?>">Ver criador</a><br/>
-          <a href="">Convidar Amigo</a><br/>
-          <?php if(isset($_SESSION['cpf']) && isset($_SESSION['senha'])){
-            echo '<a id="botaoDenunciar">Denunciar</a>';
-          } ?>
-          <div class="panel panel-primary" id="formDenuncia" >
-            <form action="rotas.php" method="post">
-              <input type="hidden" name="idCampanha" value="<?php echo $campanha->getIdCampanha(); ?>">
-              <input type="hidden" name="cpf" value="<?php echo $_SESSION['cpf']; ?>">
-              <div class="form-group col-md-12">
-                <label>Motivo</label><br>
-                <input type="radio" name="motivo" value="Campanha Duvidosa" checked> Campanha Duvidosa <br>
-                <input type="radio" name="motivo" value="Usuário Falso	"> Usuário Falso <br>
-                <input type="radio" name="motivo" value="Informações Incompletas"> Informações Incompletas	<br>
-
-              </div>
-              <div class="form-group col-md-12">
-                <label>Descrição</label>
-                <input type="text-area" class="form-control" name="descricao" required placeholder="Descricao">
-              </div>
-              <div class="row text-center">
-                <div class="col-md-6">
-                  <input type="submit" class="btn btn-primary btn-block" style="margin:5px 0px 5px 0px;" name="botaoEnviarDenuncia" value="Denunciar">
-                </div>
-              </div>
-            </form>
-
-          </div>
-
-        </div>
       </div>
+    </div>
 
 
-      <div class="panel" style="padding:0px 10px 70px 10px;">
-        <center>
-          <?php
-          if(isset($_SESSION['cpf'])){
-            if($_SESSION['cpf'] && $campanha->getCriadorCpf()){
-              echo "<a href='editarCampanha.php?campanha=<?php echo $idCampanha;?>' class='btn btn-primary col-xs-12 col-md-12 disable'>Editar Campanha</a>";
-              echo "<a href='gerarRelatorio.php?campanha=<?php echo $idCampanha;?>' class='btn btn-primary col-xs-12 col-md-12 disable'>Gerar Relatórios</a>";
-            }
+    <div class="panel" style="padding:0px 10px 70px 10px;">
+      <center>
+        <?php
+        if(isset($_SESSION['cpf'])){
+          if($_SESSION['cpf'] == $campanha->getCriadorCpf()){
+            echo "<a href='editarCampanha.php?campanha=".$campanha->getIdCampanha()."' class='btn btn-primary col-xs-12 col-md-12 disable'>Editar Campanha</a>";
+            echo "<a href='gerarRelatorio.php?campanha=".$campanha->getIdCampanha()."' class='btn btn-primary col-xs-12 col-md-12 disable'>Gerar Relatórios</a>";
           }
-          if(isset($_SESSION['cpf']) && isset($_SESSION['senha'])){
-            $href = "doar.php?idCampanha=".$campanha->getIdCampanha()."&doadorcpf=".$_SESSION['cpf'];
-          }
-          else{
-            $href = "login.php";
-          }
-          ?>
-          <a href="<?php echo $href;?>" class="btn btn-primary col-xs-12 col-md-12 disable">Efetuar Doação</a>
-        </center>
-      </div>
-      <?php
-      if(SistemaController::getInstance()->verificarCampanhaMonetaria($campanha->getIdCampanha())){
+        }
+        if(isset($_SESSION['cpf']) && isset($_SESSION['senha'])){
+          $href = "doar.php?idCampanha=".$campanha->getIdCampanha()."&doadorcpf=".$_SESSION['cpf'];
+        }
+        else{
+          $href = "login.php";
+        }
         ?>
-        <!--Para doação monetária-->
-        <?php $criador = UsuarioDAO::getInstance()->buscarUsuario($campanha->getCriadorCpf());
-        $valores = $campanha->getValores();?>
-        <div class="panel panel-default">
-          <h2 class="page-header">Doacao Monetária</h2>
-          <!--
-          <div class="row">
-          <div class="col-md-3">
-          <button type="button" id="valor1" class="btn-primary btn btn-lg" value="<?php echo $valores[0].".00";?>" name="valor"><?php echo $valores[0]." R$"; ?></button>
-        </div>
+        <a href="<?php echo $href;?>" class="btn btn-primary col-xs-12 col-md-12 disable">Efetuar Doação</a>
+      </center>
+    </div>
+    <?php
+    if(SistemaController::getInstance()->verificarCampanhaMonetaria($campanha->getIdCampanha())){
+      ?>
+      <!--Para doação monetária-->
+      <?php $criador = UsuarioDAO::getInstance()->buscarUsuario($campanha->getCriadorCpf());
+      $valores = $campanha->getValores();?>
+      <div class="panel panel-default">
+        <h2 class="page-header">Doacao Monetária</h2>
+        <!--
+        <div class="row">
         <div class="col-md-3">
-        <button type="button" class="btn-primary btn btn-lg" value="<?php echo $valores[1].".00";?>" name="valor"><?php echo $valores[1]." R$"; ?></button>
+        <button type="button" id="valor1" class="btn-primary btn btn-lg" value="<?php echo $valores[0].".00";?>" name="valor"><?php echo $valores[0]." R$"; ?></button>
       </div>
       <div class="col-md-3">
-      <button type="button" class="btn-primary btn btn-lg" value="<?php echo $valores[2].".00";?>" name="valor"><?php echo $valores[2]." R$"; ?></button>
+      <button type="button" class="btn-primary btn btn-lg" value="<?php echo $valores[1].".00";?>" name="valor"><?php echo $valores[1]." R$"; ?></button>
     </div>
     <div class="col-md-3">
-    <div class="form-group">
-    <label>Valor a ser doado: <font color="FF0000">*</font></label>
-    <input id="valordoado" type="number" class="form-control" name="valordoado" placeholder="Valor da doação">
-  </div></div>
+    <button type="button" class="btn-primary btn btn-lg" value="<?php echo $valores[2].".00";?>" name="valor"><?php echo $valores[2]." R$"; ?></button>
+  </div>
+  <div class="col-md-3">
+  <div class="form-group">
+  <label>Valor a ser doado: <font color="FF0000">*</font></label>
+  <input id="valordoado" type="number" class="form-control" name="valordoado" placeholder="Valor da doação">
+</div></div>
 </div>
 -->
 <div class=" text-center img-responsive center-block ">
