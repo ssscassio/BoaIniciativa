20b8c577b963cffc6f5988a97976d80c8f574d7e
Beatriz de Brito <beatrizbritosantana@gmail.com>
2016-05-04 09:33:07 -0300

    Modificações na página home

--- view/views/home.php
+++ view/views/home.php
@@ -18,10 +18,7 @@
           <h2 class="page-header"><?php echo $usuario->getNome(); ?></h2>
         </div>
         <div class="row col-xs-12">
-          <div class="col-md-5" >
-            <img src="../img/logobi.png" class="img-responsive img-circle" alt="" />
-          </div>
-          <div class="col-md-7 panel-primary panel container">
+          <div class="col-md-9 panel-primary panel container">
             <h3 class="page-header"> Informações Pessoais</h3>
             <div class="col-xs-12">
               <label class=""> Sexo: </label> <?php if ($usuario->getSexo() =="M") {
@@ -36,60 +33,7 @@
           </div>
 
       </div>
-          <div class="row">
-            <h4 class="page-header">Endereço</h4>
-          </div>
-            <?php
-            $l = $endereco['logradouro'];
-            $n = $endereco['numero'];
-            $b = $endereco['bairro'];
-            $c = $endereco['cidade'];
-            $enderecoJunto = "$l, $n, $b, $c";
-            ?>
 
-
-            <script type="text/javascript">
-            function initialize(){
-              var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
-
-              var options = {
-                zoom: 5,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-              };
-
-              map = new google.maps.Map(document.getElementById("map"), options);
-              var endereco = "<?php echo $enderecoJunto;?>";
-              var postos = new Array(endereco);
-
-              marcacaoEndereco(postos);
-            }
-
-            function marcacaoEndereco(postos){
-              var geocoder = new google.maps.Geocoder();
-              map.setZoom(17);
-
-              geocoder.geocode({ 'address': postos[0]}, function(results, status){
-                //se status ok
-                if (status = google.maps.GeocoderStatus.OK){
-                  //pega o retorno que ehlat e long
-                  var lat = results[0].geometry.location.lat();
-                  var lng = results[0].geometry.location.lng();
-                  var latlng = new google.maps.LatLng(lat, lng);
-                  //faz marcacao
-                  var maker = new google.maps.Marker({position: latlng, map: map});
-                  map.setCenter(latlng);//coloca na posicao da marcacao
-                }
-              });
-            }
-            </script>
-            <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBtrcCnCC71lEBRbj-hM5KwlHcSppnenBI&callback=initialize" async defer></script>
-<div class="row ">
-  <div class="container" style="height:300px; max-width:650px; padding:50px 0px 0px 0px;">
-    <div id="map" style=" height: 100%; width: 100%;"></div>
-  </div>
-
-</div>
 <br><br>
 
 
