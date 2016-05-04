16106d74ca37f0281db50bfaa0a15b76f6f3b86c
Cássio Santos <sss.cassio@gmail.com>
2016-05-04 15:02:04 -0300

    Documentação de 4 das classes do Model

--- model/Meta.php
+++ model/Meta.php
@@ -1,32 +1,47 @@
 <?php
 
-	class Meta
-	{
-		private $idCampanha;
-		private $codMaterial;
-		private $quantidade;
+/**
+* Classe referente a uma meta a ser alcançada em uma campanha ou aos materiais que a campanha pode receber
+*/
+class Meta{
 
-		public function __construct($idCampanha, $codMaterial, $quantidade){
-			$this->idCampanha = $idCampanha;
-			$this->codMaterial = $codMaterial;
-			$this->quantidade = $quantidade;
-		}
+	/** Id referente a campanha que possui esta meta*/
+	private $idCampanha;
+	/** Codigo do máterial da meta*/
+	private $codMaterial;
+	/** Quantidade do material que a campanha deseja alcançar, (0 para campanha que não finaliza por meta)*/
+	private $quantidade;
 
-		public function getIdCampanha(){
-			return $this->idCampanha;
-		}
-
-		public function getCodMaterial(){
-			return $this->codMaterial;
-		}
-
-		public function getQuantidade(){
-			return $this->quantidade;
-		}
-		
-		public function setQuantidade($quantidade){
-			$this->quantidade = $quantidade;
-		}
+	/**< Construtor da classe meta
+	*	@param $idCampanha - Id da campanha que possui a meta
+	* @param $codMaterial - Codigo do material que pode ser recebido pela campanha
+	* @param $quantidade - Quantidade do material que deseja-se receber
+	**/
+	public function __construct($idCampanha, $codMaterial, $quantidade){
+		$this->idCampanha = $idCampanha;
+		$this->codMaterial = $codMaterial;
+		$this->quantidade = $quantidade;
 	}
+
+	/**
+	* Getters e Setteres
+	* Métodos de acessos e manipulação da classe Meta
+	*/
+	public function getIdCampanha(){
+		return $this->idCampanha;
+	}
+
+	public function getCodMaterial(){
+		return $this->codMaterial;
+	}
+
+	public function getQuantidade(){
+		return $this->quantidade;
+	}
+
+	public function setQuantidade($quantidade){
+		$this->quantidade = $quantidade;
+	}
+}
 
 ?>
--- model/Usuario.php
+++ model/Usuario.php
@@ -1,155 +1,170 @@
 <?php
 
+/**
+* Classe referente a usuário, contém todas as suas informações do usuário.
+*/
+class Usuario{
+	/** O nome do usuário*/
+	private $nome;
+	/** O Cpf do usuário*/
+	private $cpf;
+	/** O Email do usuário*/
+	private $email;
+	/** A senha do usuário*/
+	private $senha;
+	/** A Url da foto do usuário*/
+	private $foto;
+	/** O Sexo do usuário ("M" ou "F")*/
+	private $sexo;
+	/** A data de nascimento do usuário (dd-mm-aaaa)*/
+	private $dataNascimento;
+	/** Array contendo as informações do endereço do usuário (cep, Estado, Bairro, Cidade, longradouro, Numero, Complemento)*/
+	private $endereco = array();//Array com os campos do endereco
+	/** A classificação do usuário*/
+	private $classificacao = 0;
+	/** Status do usuário (true: bloqueado, false: desbloqueado)*/
+	private $bloqueado;
+	/** Data que o usuário foi bloqueado*/
+	private $dataBloqueio;
+	/** Latitude do endereço do usuário*/
+	private $latitude;
+	/** Longitude do endereço do usuário*/
+	private $longitude;
+
+	/** < Construtor da classe Usuário que recebe como parâmetro suas caracteristicas. */
+	public function __construct($nome, $cpf, $email, $senha, $foto, $sexo, $dataNascimento, $endereco, $classificacao, $bloqueado, $dataBloqueio)
+	{
+		$this->nome = $nome;
+		$this->cpf = $cpf;
+		$this->email = $email;
+		$this->senha = $senha;
+		$this->foto = $foto;
+		$this->sexo = $sexo;
+		$this->dataNascimento = $dataNascimento;
+		$this->endereco = $endereco;
+		$this->classificacao =  $classificacao;
+		$this->bloqueado = $bloqueado;
+		$this->dataBloqueio = $dataBloqueio;
+	}
+
 	/**
-	* Classe referente a usuário
+	* Getters e Setteres
+	* Métodos de acessos e manipulação da classe Usuario
 	*/
-	class Usuario{
+	public function setLatitude($latitude){
+		$this->latitude = $latitude;
+	}
 
-		private $nome;
-		private $cpf; //Chave primaria
-		private $email;
-		private $senha;
-		private $foto;
-		private $sexo;
-		private $dataNascimento;
-		private $endereco = array();//Array com os campos do endereco
-		private $classificacao = 0;
-		private $contaPayPal;
-		private $bloqueado;
-		private $dataBloqueio;
-		private $longitude;
-		private $latitude;
+	public function setLongitude($longitude){
+		$this->longitude = $longitude;
+	}
 
-		public function __construct($nome, $cpf, $email, $senha, $foto, $sexo, $dataNascimento, $endereco, $classificacao, $bloqueado, $dataBloqueio)
-		{
-			$this->nome = $nome;
-			$this->cpf = $cpf;
-			$this->email = $email;
-			$this->senha = $senha;
-			$this->foto = $foto;
-			$this->sexo = $sexo;
-			$this->dataNascimento = $dataNascimento;
-			$this->endereco = $endereco;
-			$this->classificacao =  $classificacao;
-			$this->bloqueado = $bloqueado;
-			$this->dataBloqueio = $dataBloqueio;
+	public function getLatitude(){
+		return $this->latitude;
+	}
+
+	public function getLongitude(){
+		return $this->longitude;
+	}
+
+	public function getCpf(){
+		return $this->cpf;
+	}
+
+	public function setEndereco($endereco){
+		$this->endereco = $endereco;
+	}
+	public function getBloqueado(){
+		return $this->bloqueado;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function getEndereco(){
+		return $this->endereco;
+	}
+
+	public function getDataBloqueio(){
+		return $this->dataBloqueio;
+	}
+
+	public function getClassificacao(){
+		return $this->classificacao;
+	}
+
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function getSenha(){
+		return $this->senha;
+	}
+
+	public function getFoto(){
+		return $this->foto;
+	}
+
+	public function getSexo(){
+		return $this->sexo;
+	}
+
+	public function getDataNascimento(){
+		return $this->dataNascimento;
+	}
+
+	public function getAgradecimentos(){
+		return $this->agradecimentos;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+
+	public function setSenha($senha){
+		$this->senha = $senha;
+	}
+
+	public function setFoto($foto){
+		$this->foto = $foto;
+	}
+
+	public function setSexo($sexo){
+		$this->sexo = $sexo;
+	}
+
+	public function setDataNascimento($dataNascimento){
+		$this->dataNascimento = $dataNascimento;
+	}
+
+	public function setContaPayPal($contaPayPal){
+		$this->contaPayPal = $contaPayPal;
+	}
+
+	public function setClassificacao($classificacao){
+		$this->classificacao = $classificacao;
+	}
+
+	public function setBloqueado($bloqueado){
+		$this->bloqueado = $bloqueado;
+	}
+
+	/**
+	* Metodo que verifica se dois usuários são iguais a partir de seus cpfs
+	* @param $usuario Usuario que está se comparando com o usuário atual
+	*/
+	public function equals($usuario){
+		if($this->cpf == $usuario->cpf){
+			return true;
 		}
-
-		public function setLatitude($latitude){
-			$this->latitude = $latitude;
-		}
-
-		public function setLongitude($longitude){
-			$this->longitude = $longitude;
-		}
-
-		public function getLatitude(){
-			return $this->latitude;
-		}
-
-		public function getLongitude(){
-			return $this->longitude;
-		}
-
-		public function getCpf(){
-			return $this->cpf;
-		}
-
-		public function setEndereco($endereco){
-			$this->endereco = $endereco;
-		}
-		public function getBloqueado(){
-			return $this->bloqueado;
-		}
-
-		public function getEmail(){
-			return $this->email;
-		}
-
-		public function getEndereco(){
-			return $this->endereco;
-		}
-
-		public function getDataBloqueio(){
-			return $this->dataBloqueio;
-		}
-
-		public function getClassificacao(){
-			return $this->classificacao;
-		}
-
-		public function getNome(){
-			return $this->nome;
-		}
-
-		public function getSenha(){
-			return $this->senha;
-		}
-
-		public function getFoto(){
-			return $this->foto;
-		}
-
-		public function getSexo(){
-			return $this->sexo;
-		}
-
-		public function getDataNascimento(){
-			return $this->dataNascimento;
-		}
-
-		public function getAgradecimentos(){
-			return $this->agradecimentos;
-		}
-
-
-		public function getContaPayPal(){
-			return $this->contaPayPal;
-		}
-
-		public function setEmail($email){
-			$this->email = $email;
-		}
-
-		public function setNome($nome){
-			$this->nome = $nome;
-		}
-
-		public function setSenha($senha){
-			$this->senha = $senha;
-		}
-
-		public function setFoto($foto){
-			$this->foto = $foto;
-		}
-
-		public function setSexo($sexo){
-			$this->sexo = $sexo;
-		}
-
-		public function setDataNascimento($dataNascimento){
-			$this->dataNascimento = $dataNascimento;
-		}
-
-		public function setContaPayPal($contaPayPal){
-			$this->contaPayPal = $contaPayPal;
-		}
-
-		public function setClassificacao($classificacao){
-			$this->classificacao = $classificacao;
-		}
-
-		public function setBloqueado($bloqueado){
-			$this->bloqueado = $bloqueado;
-		}
-
-		public function equals($usuario){
-			if($this->cpf == $usuario->cpf){
-				return true;
-			}
-			else {
-				return false;
-			}
+		else {
+			return false;
 		}
 	}
- ?>
+}
+?>
