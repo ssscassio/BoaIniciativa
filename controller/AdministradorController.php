e9fa5920b91a0a50743df5024dce507246f08de9
Beatriz de Brito <beatrizbritosantana@gmail.com>
2016-05-04 21:54:51 -0300

    Documentação de Administradorcontroller.php

--- controller/AdministradorController.php
+++ controller/AdministradorController.php
@@ -7,8 +7,9 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."database/Administrad
 require_once($_SERVER["DOCUMENT_ROOT"]."/BoaIniciativaV3/"."database/DenunciaDAO.php");
 
 /**
- *
- */
+*Classe AdministradorController
+* Classe responsável por controlar todas as solicitações referentes ao administrador.
+*/
 class  AdministradorController
 {
 
@@ -24,11 +25,25 @@ class  AdministradorController
     return self::$instance;
   }
 
+  /** Método responsável por autenticar um administrador
+  * @param $cpfAdm cpf do administrador
+  * @param $senhaAdm senha do administrador
+  * @return retorna confirmação se ele foi autenticado ou não
+  */
   public function autenticarAdm($cpfAdm, $senhaAdm){
     return AdministradorDAO::getInstance()->autenticarAdministrador($cpfAdm,$senhaAdm);
 
   }
 
+  /** Método responsável por cadastrar um administrador
+  * @param $nome nome do administrador  que será cadastrado
+  * @param $cpf cpf do administrador que será cadastrado
+  * @param $email email do administrador que será cadastrado
+  * @param $senha senha do administrador que será cadastrado
+  * @param $nascimento nascimento do administrador que será cadastrado
+  * @param $sexo sexo do administrador que será cadastrado
+  * @return retorna confirmação se ele foi autenticado ou não
+  */
   public function cadastrarAdministrador($nome,$cpf,$email,$senha,$nascimento,$sexo){
 
     $adm = new Administrador($cpf,$nome,$senha,$email,$sexo,$nascimento);
@@ -44,6 +59,11 @@ class  AdministradorController
 
   }
 
+  /** Método responsável por listar denúncias
+  * @param $tipo se a listagem é por campanhas, por denúnica ou por criador
+  * @param $id id usado para buscar denúncias
+  * @return retorna uma lista de denúncias
+  */
   public function listarDenuncias($tipo, $id){
     if($tipo == 0){//listarTodasDenuncias
       return DenunciaDAO::getInstance()->listarDenuncias();
@@ -65,6 +85,10 @@ class  AdministradorController
 
   }
 
+  /** Método responsável por lista usuários
+  * @param $tipo define se serão listados usuários bloqueados ou todos
+  * @return retorna uma lista de usuários
+  */
   public function listarUsuarios($tipo){
     if($tipo == "todos"){//listarTodas Usuarios
       return UsuarioDAO::getInstance()->listarUsuarios();
@@ -73,6 +97,10 @@ class  AdministradorController
     }
   }
 
+  /** Método responsável por verificar quantas denúnicas possui uma campanha
+  * @param $idcampanha id da campanha que se deseja buscar a quantidade de denúncias
+  * @return a quantidade de denúncias de uma campanha
+  */
   public function numDenunciasCampanha($idCampanha){
     $denuncias = DenunciaDAO::getInstance()->buscarDenunciasPorIdCampanha($idCampanha);
     if($denuncias == null){
@@ -82,6 +110,10 @@ class  AdministradorController
     }
   }
 
+  /** Método responsável por lista a quantidade de denúncias de um usuário
+  * @param $cpfDenunciador cpf do usuário que realizou as denúncias
+  * @return retorna a quantidade de denúncias
+  */
   public function numDenunciasDenunciador($cpfDenunciador){
     $denuncias = DenunciaDAO::getInstance()->buscarDenunciasEfetuadas($cpfDenunciador);
     if($denuncias == null){
@@ -91,6 +123,10 @@ class  AdministradorController
     }
   }
 
+  /** Método responsável por lista a quantidade de denúncias de um criador
+  * @param $cpfCriador cpf do criador
+  * @return retorna a quantidade de denúncias
+  */
   public function numDenunciasCriador($cpfCriador){
     $denuncias =  AdministradorController::getInstance()->listarDenuncias(3, $cpfCriador);
     if($denuncias == null){
@@ -100,6 +136,10 @@ class  AdministradorController
     }
   }
 
+  /** Método responsável por lista a quantidade de campanhas criadas por um usuário
+  * @param $cpfCriador cpf do criador
+  * @return retorna a quantidade de campanhas
+  */
   public function numCampanhasCriador($cpfCriador){
     $campanhas = CampanhaDAO::getInstance()->buscarCampanhaPorCriador($cpfCriador);
     if($campanhas == null){
@@ -109,14 +149,25 @@ class  AdministradorController
     }
   }
 
+  /** Método responsável por buscar um usuário
+  * @param $cpfUsuario cpf do usuário
+  * @return retorna as informações do usuário buscado
+  */
   public function buscarUsuario($cpfUsuario){
     return UsuarioDAO::getInstance()->buscarUsuario($cpfUsuario);
   }
 
+  /** Método responsável por buscar campanhas criadas por um determinado usuário
+  * @param $cpfUsuario cpf do usuário
+  * @return retorna uma lista de campanhas
+  */
   public function buscarCampanhaPorCriador($cpfUsuario){
     return CampanhaDAO::getInstance()->buscarCampanhaPorCriador($cpfUsuario);
   }
 
+  /** Método responsável por bloquear um usuário
+  * @param $cpfUsuario cpf do usuário que deve ser bloqueado
+  */
   public function bloquearUsuario($cpfUsuario){
 
     $date = new DateTime();
@@ -125,6 +176,11 @@ class  AdministradorController
 
   }
 
+  /** Método responsável listar campanhas
+  * @param $id id do usuário
+  * @param $tipo verifica se a listagem é por criador, por campanhas finalizadas ou de todos os usuários
+  * @return retorna uma lista de campanhas
+  */
   public function listarCampanhas($tipo, $id){
     $campanhas = array();
     if($tipo == 0){//listarTodosUsuarios
@@ -150,10 +206,17 @@ class  AdministradorController
     return $campanhas;
   }
 
+  /** Método responsável desbloquear um usuário
+  * @param $cpfUsuario cpf do usuário que deverá ser desbloqueado
+  */
   public function desbloquearUsuario($cpfUsuario){
     UsuarioDAO::getInstance()->desbloquearUsuario($cpfUsuario);
   }
 
+  /** Método responsável excluir um usuário
+  * @param $cpfUsuario cpf do usuário que deverá ser excluido
+  * @return retorna uma confirmação se o usuário ainda existe ou não
+  */
   public function excluirUsuario($cpfUsuario){
     UsuarioDAO::getInstance()->deletarUsuario($cpfUsuario);
     $usuario = UsuarioDAO::getInstance()->buscarUsuario($cpfUsuario);
@@ -161,6 +224,9 @@ class  AdministradorController
 
   }
 
+  /** Método responsável encerrar uma campanha
+  * @param $idCampanha id da campanha que deverá ser encerrada
+  */
   public function encerrarCampanha($idCampanha){
     CampanhaDAO::getInstance()->encerrarCampanha($idCampanha, date('d/m/Y'));
   }
